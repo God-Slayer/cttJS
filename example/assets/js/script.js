6b7919502e97031e74cf2b9be99a789efa21cdf4
God-Slayer <60921940+God-Slayer@users.noreply.github.com>
2021-10-12 12:02:40 +0200

    Update to v1.0.1

--- example/assets/js/script.js
+++ example/assets/js/script.js
@@ -1,33 +1,44 @@
 $(document).ready(function(){
-  cttJS.Initialize();
+  cttJS.Initialize({
+    background: 'rgba(0,153,255,.8)',
+    color: 'rgb(255,255,255)',
+    font_size: '.8rem',
+    text_align: 'center',
+    text_transform:'capitalize',
+    border_radius: '.2rem',
+    padding: '.3rem'
+  });
   cttJS.CreateTooltip('.vert_elem',{
-    text:'this is vertically aligned',
-    align:'vertical',
+    text:'this is vertically aligned with styling overridden',
+    align:'v',
     offset:{
-      v_align:{
-        x:10,
-        y:32
-      }
+      x:10,
+      y:32
+    },
+    delay:500,
+    style:{
+      background:'#e36363'
     }
   });
   cttJS.CreateTooltip('.hori_elem',{
     text:'this is horizontally aligned',
     align:'horizontal',
     offset:{
-      h_align:{
-        x:20,
-        y:0
-      }
-    }
+      x:20,
+      y:0
+    },
+    delay:1000
   });
-  cttJS.CreateTooltip('#third',{
-    text:'third tooltip',
+  cttJS.CreateTooltip('#custom',{
+    text:`<div class="custom_el">
+      <i class="fas fa-brain"></i>
+      <span>Big brain time baby</span>
+    </div>`,
     align:'h',
     offset:{
-      h_align:{
-        x:20,
-        y:0
-      }
-    }
+      x:20,
+      y:0
+    },
+    delay:1500
   });
 });
